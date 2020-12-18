c1a676d6d619b50c4721c5beec5f8d77e44dd945
io+_ <40149230+ioDar65@users.noreply.github.com>
2020-12-18 21:12:54 +0300

    Create LabClass.java

---
+++ lab9/LabClass.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class LabClass {
+    final private LabClassUI classUI;
+
+    public LabClass() {
+        this.classUI = new LabClassDriver();
+    }
+
+    public LabClassUI getClassUi() {
+        return classUI;
+    }
+
+    @Override
+    public String toString() {
+        return classUI.toString();
+    }
+}
