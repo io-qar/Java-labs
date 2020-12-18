0cb47baccdc2eed2f4936bfcb0d341ad9659fe2e
io+_ <40149230+ioDar65@users.noreply.github.com>
2020-12-18 21:19:14 +0300

    Create AbstractChairFactory.java

---
+++ lab10/AbstractChairFactory.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public interface AbstractChairFactory {
+    VictorianChair createVictorianChair();
+    MagicChair createMagicChair();
+    FunctionalChair createFunctionalChair();
+}
